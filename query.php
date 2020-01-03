d1c700785e2bd75635524c0cf3ae200fac4fe99f
ahmmed <ahmmedmohammed90x@hotmail.com>
2020-01-02 21:26:36 -0700

    tip8

--- query.php
+++ query.php
@@ -3,7 +3,7 @@
 require 'classes.php';
 
 if ( $xlsx = SimpleXLSX::parse('data.xlsx') ) {
-   // getting user input
+   // getting user's input
    $value = $_POST['input'];
  // fetching the data from excel sheet
    foreach( $xlsx->rows() as $r ) {
@@ -13,23 +13,17 @@ if ( $xlsx = SimpleXLSX::parse('data.xlsx') ) {
 
 
    }
-
-
 }else {
    echo SimpleXLSX::parseError();
 }
 
-// creat a function that loop throw the days and print it 
+// creat a function that loop throw the days and print them and show the availablilty
 function checkRow($r){
     $list =['Sunday','Monday','Tuesday','Wedsday','Thursday','Friday', 'Saturday' ];
-
     $i = 0 ;
-
     for($i=0 ; $i<= 6 ; $i++ ){
         if($r[$i+6] == 'X'){
             echo '<p>'.$list[$i].'</p>' .'<div> <p style="color:green;"> available </p></div>';
-
-
         }
         else{
             echo '<p>'.$list[$i].'</p>' .'<div> <p style="color:red;"> unavailable </p></div>';
@@ -37,8 +31,6 @@ function checkRow($r){
         }
     }
 
-
 }
-
 
  ?>
