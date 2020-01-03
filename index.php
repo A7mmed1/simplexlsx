f739c886a319c1caad9eff680d0b55bd2dc4efaa
ahmmed <ahmmedmohammed90x@hotmail.com>
2020-01-02 20:03:46 -0700

    h

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
          <h1>Challenge</h1>
 
           <div class="">
-              <form class="" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
+              <form class="" method="post" action="query.php" >
 >
                    <p> Postal code</p><input type="text" name="input" value="">
                    <input type="submit" name="" value="Check">
