3bb6ff3f84cfb6a6b1b2e85b4611656993266418
ahmmed <ahmmedmohammed90x@hotmail.com>
2020-01-02 20:34:23 -0700

    color

--- query.php
+++ query.php
@@ -15,28 +15,24 @@ if ( $xlsx = SimpleXLSX::parse('data.xlsx') ) {
            echo '<table border="1" cellpadding="3" style="border-collapse: collapse">';
            if($r[6] == x ){
                 echo 'green';
-               echo '<tr><td>'.implode('</td><td>', $r[6]).'</td></tr>';
-               echo '</table>';
+
            }
            else{
                echo 'red';
            }
            if($r[7] == x ){
                 echo 'green';
-               echo '<tr><td>'.implode('</td><td>', $r[6]).'</td></tr>';
-               echo '</table>';
            }
            else{
                echo 'red';
            }
            if($r[8] == x ){
                echo 'green';
-               echo '<tr><td>'.implode('</td><td>', $r[6]).'</td></tr>';
-               echo '</table>';
            }
            else{
                echo 'red';
            }
+            echo '</table>';
 
        }
 
